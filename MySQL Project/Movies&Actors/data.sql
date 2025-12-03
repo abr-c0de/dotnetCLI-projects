af65ea65e1b009e5339be3ba3025f18a13ad192e
Abhishek <abhishek1nett@gmail.com>
2025-12-03 18:49:36 +0530

    mysqlprojectadded

---
+++ MySQL Project/Movies&Actors/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO Movies(Title,ReleaseYear,Genre) VALUES
+('Inception','2010-02-12','Sci-Fi'),
+('Interstellar','2014-07-14','Sci-Fi'),
+('The Dark Knight','2008-06-08','Action'),
+('Ironman','2012-09-10','Sci-Fi'),
+('Avatar','2009-05-08','Adventure');
+
+
+
+
+INSERT INTO Actors(Name) VALUES
+('Leonardo DiCaprio'),
+('Matthew McConaughey'),
+('Christian Bale'),
+('Anne Hathaway'),
+('Rober Downy Jr'),
+('Scarlet Johanson'),
+('Bradley Cooper');
+
+
+
+INSERT INTO Casting(MovieId,ActorId,RoleName,Salary) VALUES
+(4,5,'Hero',2100000),
+(4,6,'Side Actor',350000),
+(5,7,'Villain',3200000);
